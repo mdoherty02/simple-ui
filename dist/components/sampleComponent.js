ff851d6b46d44c98606fed706d508e3c96fa3a79
Matt Doherty <matt.doherty.02@gmail.com>
2017-07-08 17:39:42 -0400

    Initial commit for the simple-ui project

---
+++ dist/components/sampleComponent.js
@@ -0,0 +1,246 @@
+var SimpleUI = (function () {
+'use strict';
+
+function noop() {}
+
+function assign(target) {
+	var arguments$1 = arguments;
+
+	var k,
+		source,
+		i = 1,
+		len = arguments.length;
+	for (; i < len; i++) {
+		source = arguments$1[i];
+		for (k in source) { target[k] = source[k]; }
+	}
+
+	return target;
+}
+
+function appendNode(node, target) {
+	target.appendChild(node);
+}
+
+function insertNode(node, target, anchor) {
+	target.insertBefore(node, anchor);
+}
+
+function detachNode(node) {
+	node.parentNode.removeChild(node);
+}
+
+function createElement(name) {
+	return document.createElement(name);
+}
+
+function createText(data) {
+	return document.createTextNode(data);
+}
+
+function setAttribute(node, attribute, value) {
+	node.setAttribute(attribute, value);
+}
+
+function differs(a, b) {
+	return a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+}
+
+function dispatchObservers(component, group, newState, oldState) {
+	for (var key in group) {
+		if (!(key in newState)) { continue; }
+
+		var newValue = newState[key];
+		var oldValue = oldState[key];
+
+		if (differs(newValue, oldValue)) {
+			var callbacks = group[key];
+			if (!callbacks) { continue; }
+
+			for (var i = 0; i < callbacks.length; i += 1) {
+				var callback = callbacks[i];
+				if (callback.__calling) { continue; }
+
+				callback.__calling = true;
+				callback.call(component, newValue, oldValue);
+				callback.__calling = false;
+			}
+		}
+	}
+}
+
+function get(key) {
+	return key ? this._state[key] : this._state;
+}
+
+function fire(eventName, data) {
+	var this$1 = this;
+
+	var handlers =
+		eventName in this._handlers && this._handlers[eventName].slice();
+	if (!handlers) { return; }
+
+	for (var i = 0; i < handlers.length; i += 1) {
+		handlers[i].call(this$1, data);
+	}
+}
+
+function observe(key, callback, options) {
+	var group = options && options.defer
+		? this._observers.post
+		: this._observers.pre;
+
+	(group[key] || (group[key] = [])).push(callback);
+
+	if (!options || options.init !== false) {
+		callback.__calling = true;
+		callback.call(this, this._state[key]);
+		callback.__calling = false;
+	}
+
+	return {
+		cancel: function() {
+			var index = group[key].indexOf(callback);
+			if (~index) { group[key].splice(index, 1); }
+		}
+	};
+}
+
+function on(eventName, handler) {
+	if (eventName === 'teardown') { return this.on('destroy', handler); }
+
+	var handlers = this._handlers[eventName] || (this._handlers[eventName] = []);
+	handlers.push(handler);
+
+	return {
+		cancel: function() {
+			var index = handlers.indexOf(handler);
+			if (~index) { handlers.splice(index, 1); }
+		}
+	};
+}
+
+function set(newState) {
+	this._set(assign({}, newState));
+	this._root._flush();
+}
+
+function _flush() {
+	var this$1 = this;
+
+	if (!this._oncreate) { return; }
+
+	while (this._oncreate.length) {
+		this$1._oncreate.pop()();
+	}
+}
+
+var proto = {
+	get: get,
+	fire: fire,
+	observe: observe,
+	on: on,
+	set: set,
+	_flush: _flush
+};
+
+var template = (function () {
+return {
+    data: function() {
+        return {
+          text: 'Sample'
+        };
+    }
+};
+}());
+
+function add_css () {
+	var style = createElement( 'style' );
+	style.id = 'svelte-2616562917-style';
+	style.textContent = "\n[svelte-2616562917].sample__text, [svelte-2616562917] .sample__text {\n\tcolor: \\#f00;\n}\n";
+	appendNode( style, document.head );
+}
+
+function create_main_fragment ( state, component ) {
+	var h1, text_value, text;
+
+	return {
+		create: function () {
+			h1 = createElement( 'h1' );
+			text = createText( text_value = state.text );
+			this.hydrate();
+		},
+
+		hydrate: function ( nodes ) {
+			setAttribute( h1, 'svelte-2616562917', '' );
+			h1.className = "sample__text";
+		},
+
+		mount: function ( target, anchor ) {
+			insertNode( h1, target, anchor );
+			appendNode( text, h1 );
+		},
+
+		update: function ( changed, state ) {
+			if ( text_value !== ( text_value = state.text ) ) {
+				text.data = text_value;
+			}
+		},
+
+		unmount: function () {
+			detachNode( h1 );
+		},
+
+		destroy: noop
+	};
+}
+
+function SampleComponent_temp ( options ) {
+	options = options || {};
+	this._state = assign( template.data(), options.data );
+
+	this._observers = {
+		pre: Object.create( null ),
+		post: Object.create( null )
+	};
+
+	this._handlers = Object.create( null );
+
+	this._root = options._root || this;
+	this._yield = options._yield;
+
+	this._torndown = false;
+	if ( !document.getElementById( 'svelte-2616562917-style' ) ) { add_css(); }
+
+	this._fragment = create_main_fragment( this._state, this );
+
+	if ( options.target ) {
+		this._fragment.create();
+		this._fragment.mount( options.target, null );
+	}
+}
+
+assign( SampleComponent_temp.prototype, proto );
+
+SampleComponent_temp.prototype._set = function _set ( newState ) {
+	var oldState = this._state;
+	this._state = assign( {}, oldState, newState );
+	dispatchObservers( this, this._observers.pre, newState, oldState );
+	this._fragment.update( newState, this._state );
+	dispatchObservers( this, this._observers.post, newState, oldState );
+};
+
+SampleComponent_temp.prototype.teardown = SampleComponent_temp.prototype.destroy = function destroy ( detach ) {
+	this.fire( 'destroy' );
+
+	if ( detach !== false ) { this._fragment.unmount(); }
+	this._fragment.destroy();
+	this._fragment = null;
+
+	this._state = {};
+	this._torndown = true;
+};
+
+return SampleComponent_temp;
+
+}());
